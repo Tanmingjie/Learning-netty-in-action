7e43f7a78cb4cebb7c67df540c0105cbc3fd59e8
Oliver Tan <olivert@synnex.com>
2019-03-28 17:56:34 +0800

    My netty-in-action

---
+++ chapter1/src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Olivert on 03/28/19.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        String arr = new String("      1 2 3    ");
+        System.out.println(arr.trim());
+    }
+}
---
+++ chapter2/Client/src/main/java/EchoClientHandler.java
@@ -0,0 +1,29 @@
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+/**
+ * Created by Olivert on 03/27/19.
+ */
+@Sharable
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) {
+        System.out.println("Client received: " + msg.toString(CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
